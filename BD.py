5f996c96185697d57ad00697f66ddf6ed87a5195
Abisaid Munguia <anguloabisaid@gmail.com>
2025-10-18 13:58:24 -0700

    Agregar Funciones Para la BD

--- BD.py
+++ BD.py
@@ -1,12 +1,12 @@
 import sqlite3 as sql
 
 def crear_BD():
-        conexion = sql.Connection("CalzaIsoft.db")
+        conexion = sql.connect("CalzaIsoft.db")
         conexion.commit()
         conexion.close()
 
 def crear_Tabla():
-        conexion = sql.Connection("CalzaIsoft.db")
+        conexion = sql.connect("CalzaIsoft.db")
         cursor = conexion.cursor()
         cursor.execute(
         """ CREATE TABLE Administradores  (
@@ -17,9 +17,59 @@ def crear_Tabla():
         conexion.close()
 
 def alterar():
-        conexion = sql.Connection("CalzaIsoft.db")
+        conexion = sql.connect("CalzaIsoft.db")
         cursor = conexion.cursor()
         conexion.commit()
         conexion.close()
 
-crear_Tabla()
+def agregar_nuevo_zapato(tabla,id,sexo,talla,color,material,tipo,precio,stock):
+        conexion = sql.connect("CalzaIsoft.db")
+        cursor = conexion.cursor()
+        cursor.execute(f"INSERT INTO {tabla} VALUES ('{id}','{sexo}',{talla},'{color}','{material}','{tipo}',{precio},{stock})")
+        conexion.commit()
+        conexion.close()
+
+def mostrar_datos(Tabla):
+        conexion = sql.connect("CalzaIsoft.db")
+        cursor = conexion.cursor()
+        cursor.execute(f"Select * from {Tabla}")
+        filas = cursor.fetchall()
+
+        for filas in filas:
+                print(filas)
+        
+        conexion.close()
+
+def Eliminar_Zapato(Tabla,Id):
+        conexion = sql.connect("CalzaIsoft.db")
+        cursor = conexion.cursor()
+        cursor.execute(f"DELETE FROM {Tabla} WHERE Id_Producto = '{Id}'")
+        conexion.commit()
+        conexion.close()
+
+def Agregar_Stock(Tabla,agregado,id):
+        conexion = sql.connect("CalzaIsoft.db")
+        cursor = conexion.cursor()
+        cursor.execute(f"""
+        UPDATE {Tabla}
+        SET Stock = Stock + {agregado}
+        WHERE Id_Producto = '{id}'
+        """)
+        conexion.commit()
+        conexion.close()
+
+def Eliminar_Stock(Tabla,eliminado,id):
+        conexion = sql.connect("CalzaIsoft.db")
+        cursor = conexion.cursor()
+        cursor.execute(f"""
+        UPDATE {Tabla}
+        SET Stock = Stock - {eliminado}
+        WHERE Id_Producto = '{id}'
+        """)
+        conexion.commit()
+        conexion.close()
+
+
+
+
+
